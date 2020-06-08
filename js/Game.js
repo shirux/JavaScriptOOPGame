81cfdbd2d59d11665268e014cd3af78d9fc51ea4
shirux <shirux123@gmail.com>
2020-06-08 18:18:03 -0500

    classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,60 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+/**
+ * 
+ */
+ class Game {
+    /**
+     * 
+     */
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            'Honey I am busy',
+            'Do not worry be happy',
+            'I love javascript',
+            'That is one small step for man one giant leap for mankind',
+            'SpaceX has launched javascript to outer space'
+        ];
+        this.activePhrase = null;
+    }
+
+    /**
+     * Starts a game by hiding the overlay, selecting a random phrase and displaying it.
+     */
+    startGame() {
+        const overlay = document.querySelector('#overlay');
+        overlay.style.display = 'none';
+
+        this.activePhrase = new Phrase(this.getRandomPhrase());
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    /**
+     * Return a random phrase from the phrases array
+     * @return {string} Random phrase
+     */
+    getRandomPhrase() {
+        let random = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[random];
+    }
+
+    handleInteraction() {
+        //TODO
+    }
+
+    removeLife() {
+        //TODO
+    }
+
+    checkForWin() {
+        //TODO
+    }
+
+    checkForWin() {
+        //TODO
+    }
+
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,63 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ /**
+  * Phrase class
+  */
+ class Phrase {
+     /**
+      * 
+      * @param {*} phrase 
+      */
+    constructor(text) {
+        this.phrase = text;
+    }
+
+    /**
+     * 
+     */
+    addPhraseToDisplay() {
+        const ulPhrase = document.querySelector('#phrase ul');
+        ulPhrase.innerHTML = '';
+
+        [...this.phrase].forEach(letter => {
+            const li = document.createElement('li');
+            li.textContent = letter;
+            if (letter === ' ') {
+                li.classList.add('space');
+            } else {
+                li.classList.add('hide');
+                li.classList.add('letter');
+                li.classList.add(letter);
+            }
+            ulPhrase.appendChild(li);
+        });
+    }
+
+    /**
+     * 
+     * @param {*} letter 
+     */
+    checkLetter(letter) {
+        let result = [];
+        [...this.phrase].forEach((char, index) => {
+            if (char === letter) result.push(index);
+        });
+        return result;
+    }
+
+    /**
+     * 
+     * @param {*} letter 
+     */
+    showMatchedLetter(letter){
+        const matches = document.querySelectorAll(`.${letter}`);
+        matches.forEach(match => {
+            match.classList.remove('hide');
+            match.classList.add('show');
+        })
+    }
+
+
+ }
